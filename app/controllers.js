d7821e742ae862d9da69d11f3ab74562a28d7476
Kasun <kasunedward48@gmail.com>
2017-04-01 10:37:50 +0530

    modified

--- app/controllers.js
+++ app/controllers.js
@@ -89,6 +89,16 @@ angular.module('myApp.controllers',['cordovaGeolocationModule'])
         
 
         $scope.map = new google.maps.Map(document.getElementById("gmaps"), mapOptions);
+        $http({
+            method: 'GET',
+            url: '/someUrl'
+        }).then(function successCallback(response) {
+            // this callback will be called asynchronously
+            // when the response is available
+        }, function errorCallback(response) {
+            // called asynchronously if an error occurs
+            // or server returns response with an error status.
+        });
 
     }, function(error){
         console.log(error);
